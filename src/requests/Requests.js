6a552e1f6c86c60df65e7291b7fb57b611fe9111
koechslin <killian.oechslin@insa-lyon.fr>
2020-11-17 16:13:24 +0100

    Remove unused request

--- src/requests/Requests.js
+++ src/requests/Requests.js
@@ -88,52 +88,6 @@ export async function fetchReversedSearchResult(userEntry, onResultsFound) {
 	}
 }
 
-export async function fetchSearchResultsDisease(userEntry, onResultsFound) {
-	try {
-		let requestUrl =
-			"http://dbpedia.org/sparql/?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=";
-		let suffixUrl =
-			"&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+";
-		let query =
-			` SELECT ?s as ?search ?nameFr ?nameEn ?commentFr ?commentEn ?image
-        WHERE {
-            ?s a yago:Disease114070360;
-            foaf:depiction ?image
-            FILTER regex(?s, "` +
-			userEntry +
-			`", "i").
-            OPTIONAL {
-                ?s rdfs:label ?nameFr.
-                FILTER(langMatches(lang(?nameFr), "fr")).
-            }
-            OPTIONAL {
-                ?s rdfs:label ?nameEn.
-                FILTER(langMatches(lang(?nameEn), "en")).
-            }
-            OPTIONAL {
-                ?s rdfs:comment ?commentFr.
-                FILTER(langMatches(lang(?commentFr), "fr")).
-            }
-            OPTIONAL {
-                ?s rdfs:comment ?commentEn.
-                FILTER(langMatches(lang(?commentEn), "en")).
-            }
-        } LIMIT 10 `;
-		fetch(requestUrl + query + suffixUrl)
-			.then((res) => res.json())
-			.then(
-				(result) => {
-					onResultsFound(result.results.bindings, userEntry);
-				},
-				(error) => {
-					console.log("Error : ", error);
-				}
-			);
-	} catch (err) {
-		console.log("something went wrong", err);
-	}
-}
-
 export function fetchAllInfosGenes(idD) {
 	const sparqlQueryDisgenet =
 		`
